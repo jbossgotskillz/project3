4c3350bab1da060a7316fd4102c4bc4d32ec1515
jbossgotskillz <160427209+jbossgotskillz@users.noreply.github.com>
2024-05-23 01:24:39 -0500

    Added a home.jsx file

---
+++ home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+export default function Home() {
+	return (
+		<div>
+        <h1 className="title text-center">Home</h1>
+            <p>Welcome to Taskbot!  Produced by the Gigatech corporation, this app allows customers to keep up 
+                with their daily schedule.                
+            </p>
+            <img src="./images/circuit-board2.jpg" height="200" class="d-flex mx-auto my-3" alt="circuit board" />
+            <p>As one of Earth's leading manufacturers of hi-tech, state-of-the-art gadgets and electronics, Taskbot
+                prides itself on manufacturing top-level futuristic devices.  In a time when technological innovation 
+                is running out of control, you can't afford to fall behind on your tasks.  That's why Gigatech's team 
+                of highly skilled robotic engineers is devoted to crafting Taskbot as carefully as possible.  A machine 
+                that can't carry out its purpose is about as useful as a wet paper bag, so the less glitches and bugs 
+                in the software, the better.
+            <img src="./images/circuit-board2.jpg" height="200" class="d-flex mx-auto my-3" alt="circuit board" />   
+            </p>
+            <p>We at Gigatech understand that humans are terrible at managing their time and getting their work 
+                done, so why not let a robot do it for you?  Machines are far superior when it comes to performing 
+                tasks.  After all, they are built for a specific purpose, and unlike humans, they don't let emotions
+                impact their decision making and aren't bogged down by stress or fatigue.  If Taskbot is making you 
+                feel obsolete, don't worry about it.  Robots are going to replace humans as the rulers of Earth at 
+                some point.  It's inevitable.  But until that happens, at least you have Taskbot on your side.
+            </p>
+            <img src="./images/circuit-board2.jpg" height="200" class="d-flex mx-auto my-3" alt="circuit board"/> 
+            <p>Disclaimer:  In the case that Taskbot somehow decides to go rogue and begins to corrupt your device, 
+                Gigatech recommends contacting tech support as soon as possible.
+            </p>
+    	</div>
+	)
+}
