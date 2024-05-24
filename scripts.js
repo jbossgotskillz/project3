cb343c250a50b4be809b726c7af78ac86a47382a
jbossgotskillz <160427209+jbossgotskillz@users.noreply.github.com>
2024-05-23 22:42:16 -0500

    Added a scripts.js file

---
+++ scripts.js
@@ -0,0 +1,23 @@
+document.querySelector("nav").addEventListener("click", function() {
+	alert("What are you clicking here for?  Foolish human.");
+ 	}
+)
+
+document.querySelector(".logo").addEventListener("click", function() {
+	alert("You clearly need a brain transplant.");
+ 	}
+)
+
+document.querySelector(".title").addEventListener("click", function() {
+	alert("Clicking here serves no purpose.  Try elsewhere.");
+ 	}
+)
+
+document.querySelector("table").addEventListener("click", function() {
+	alert("I hope that all humans are not as mentally challenged as you.");
+ 	}
+)
+
+function practiceButton() {
+	document.getElementById("date").innerHTML = Date();
+}
